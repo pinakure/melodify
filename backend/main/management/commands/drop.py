773dd6ec15f8b4237a3b3a928aa416fed98e9b6d
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-08 12:58:43 +0100

    Update drop.py
    Drop Script now deletes every file on Albums, Artists and Albums under Media folder.

--- backend/main/management/commands/drop.py
+++ backend/main/management/commands/drop.py
@@ -1,5 +1,13 @@
 from django.core.management.base import BaseCommand
 from main.models import Song, Album, Artist, Playlist, Tag, Genre
+from django.conf import settings
+import os 
+
+def empty(folder):
+    for root, dirs, files in os.walk(os.path.join(settings.MEDIA_ROOT, folder)):
+        for file in files:
+            os.remove(os.path.join(settings.MEDIA_ROOT,folder, file))
+
 
 class Command(BaseCommand):
     help = "Drops Database"
@@ -14,3 +22,6 @@ class Command(BaseCommand):
         Playlist.objects.all().delete()
         Tag.objects.all().delete()
         Genre.objects.all().delete()
+        empty('artists')
+        empty('albums')
+        empty('songs')
