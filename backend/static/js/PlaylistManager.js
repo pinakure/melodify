47a64abb10897986343447ed3ca3531338ce710f
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-10 13:33:07 +0100

    Playlist Responsiveness

--- backend/static/js/PlaylistManager.js
+++ backend/static/js/PlaylistManager.js
@@ -2,19 +2,31 @@ function PlaylistManager(){
 
 }
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
+function hidePlaylists( ){
+    const div = document.getElementById('playlists-window');
+    div.style.display = 'none';
+}
+
+function showPlaylists( parent, song_id ){
+    const div = document.getElementById('playlists-window');
+    div.style.display = 'inline-block';
+
+    /* Do stuff */
+    document.getElementById('playlists-window').setAttribute('data-song', song_id);
+}
+
+function enqueueSong( artist, title, filename, node_id, song_id ){
+    alert(`Enqueue ${title}...`);
+    var song = {
+        title   : `${artist}`,
+        file    : filename,
+        howl    : null,
+        next    : melodify_player.playlist[ melodify_player.index ].next ?? null,
+    };
+    melodify_player.enqueue(song); 
+    melodify_player.playlist[ melodify_player.playlist.count-2 ].next = melodify_player.playlist[ melodify_player.playlist.count-2 ].next; 
+    
+    melodify_player.playlist[ melodify_player.index ].next = song; ç
 }
 
 PlaylistManager.prototype.addSongToPlaylist = function(playlist_id){
@@ -44,6 +56,21 @@ PlaylistManager.prototype.addSongToPlaylist = function(playlist_id){
         alert('Ocurrió un error de red o del servidor.');
     });
 
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 
 const playlistmgr = new PlaylistManager();
