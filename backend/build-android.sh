7e70819d7694706fee3b64734e9da4be9baf226a
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-14 12:35:48 +0100

    Update build-android.sh

--- backend/build-android.sh
+++ backend/build-android.sh
@@ -2,3 +2,4 @@
 source ../venv/bin/activate
 python -m pip install cython django Pillow kivy kivymd mutagen requests buildozer
 python -m buildozer android debug
+cp bin/melodify-0.1-arm64-v8a_armeabi-v7a-debug.apk /mnt/c/codigo/melodify/melodify.apk
