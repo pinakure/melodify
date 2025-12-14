35fea7c95c37c8b038f0fa838c34f14d0e3c2c4d
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-14 14:36:00 +0100

    Update build-android.sh

--- backend/build-android.sh
+++ backend/build-android.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+buildozer appclean
+git stash
+git pull
 source ../venv/bin/activate
 python -m pip install cython django Pillow kivy kivymd mutagen requests buildozer
 python -m buildozer android debug
