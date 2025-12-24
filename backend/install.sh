96ffc853401b0fbaf175107472042822c4702972
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-24 11:55:42 +0100

    Update install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -3,7 +3,7 @@ USER=$(whoami)
 source ../venv/bin/activate
 echo "Installing dependencies..."
 sudo apt install nginx openjdk-17-jdk openjdk-17-jre cmake pkg-config automake autoconf libtool libffi-dev libssl-dev python3-dev libltdl-dev libsqlite3-dev
-pip install "cython<3.0.0" python-for-android "django==6.0" kivy kivymd asgiref sqlparse Pillow mutagen requests django-fontawesome_5 setuptools gunicorn buildozer
+pip install "cython<3.0.0" python-for-android django kivy kivymd asgiref sqlparse Pillow mutagen requests django-fontawesome_5 setuptools gunicorn buildozer
 echo "Installing server config files..."
 sudo cp config/etc/nginx/sites-available/melodify /etc/nginx/sites-available/melodify 
 sudo cp config/etc/systemd/system/gunicorn.service /etc/systemd/system/gunicorn.service 
