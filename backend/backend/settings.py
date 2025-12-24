6d29b10ff2036b306a3492a4ec9bb5e44b1a05be
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-24 12:17:45 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,6 +114,10 @@ USE_TZ = True
 SERVER = False
 if SERVER:
     STATIC_ROOT = '/static/melodify'
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SEGURE = True
 STATIC_FILES = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [ STATIC_FILES ]
 STATIC_URL = 'static/'
