8c4617e57f915724b4c8de972bb70107cdffc138
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2025-12-29 18:54:42 +0100

    Update settings-install.py

--- backend/backend/settings-install.py
+++ backend/backend/settings-install.py
@@ -1,32 +1,14 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-VERSION = '0.251229'
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-x-xr0dk^wcis00z=aal_0@xx0z__+eviot4wt29-*%^uvp!i*8'
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG   = False
 SECURE  = False
 ANDROID = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '192.168.1.151',
-    '192.168.1.178',
-    'localhost',
-    'melodify.com',
-    'melodify.com.82.223.13.41.nip.io',
-]
-
-
-# Application definition
+ALLOWED_HOSTS = [ '127.0.0.1' ]
 
 INSTALLED_APPS = [
     'main',
@@ -67,10 +49,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -78,11 +56,7 @@ DATABASES = {
     }
 }
 
-LOG_FILE = '\codigo\melodify\melodify.log'
-# LOG_FILE = '/var/log/melodify.log'
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+LOG_FILE = '/var/log/melodify.log'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -98,22 +72,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 if not DEBUG:
     STATIC_ROOT = '/static/melodify'
     if SECURE:
@@ -126,11 +89,6 @@ STATICFILES_DIRS = [ STATIC_FILES ]
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SPOTIFY_CLIENT_ID  = 'dc75272b15354119b9df60392848cc6a'
 SPOTIFY_CLIENT_SECRET = '76d4dfef594f4625bd68b8068a574289' 
