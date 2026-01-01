c01ff1ce26a4fda341bcacaeaecf6f9441afd7ed
Smiker <pinakure@gmail.com>
2026-01-01 23:45:19 +0100

    Fix APK

--- melodify.apk/src/melodify/app.py
+++ melodify.apk/src/melodify/app.py
@@ -8,14 +8,22 @@ from toga.style.pack import COLUMN, ROW
 
 class Melodify(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
+        # 1. Definimos la URL. 
+        # Usa 'http://10.0.2.2:8000' para conectar al Django de tu PC desde el emulador.
+        # Usa 'http://127.0.0.1:8000' solo si el servidor corre dentro del mismo dispositivo.
+        server_url = "http://127.0.0.1:8000"
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        # 2. Creamos el contenedor principal
+        main_box = toga.Box(style=Pack(direction=COLUMN))
 
+        # 3. Creamos el WebView que apuntará a tu Django
+        self.webview = toga.WebView(
+            url=server_url,
+            style=Pack(flex=1) # El flex=1 hace que ocupe toda la pantalla
+        )
+
+        # 4. Agregamos el webview al box
+        main_box.add(self.webview)
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
